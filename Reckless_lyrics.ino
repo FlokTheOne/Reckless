3946e8b76bd6f2a336745d5a5482e776b62cd1e4
FlokTheOne <im7000281@gmail.com>
2024-11-15 22:01:11 +0200

    Add files via upload

---
+++ Reckless_lyrics.ino
@@ -0,0 +1,104 @@
+// C++ code
+//
+#include <Adafruit_LiquidCrystal.h>
+
+int pressed = 0;
+
+Adafruit_LiquidCrystal lcd_1(0);
+
+
+void setup()
+{
+  pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+
+  lcd_1.begin(16, 2);
+
+}
+
+void loop()
+{
+ 
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("Each night goes by");
+  lcd_1.setBacklight(1);
+  delay(700); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("by");
+  lcd_1.setBacklight(1);
+  delay(500); // Wait for 500 millisecond(s)
+
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("And Each night");
+  lcd_1.setBacklight(1);
+ 
+    delay(700); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("I cry, Sombody");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("Saw you with her");
+  lcd_1.setBacklight(1);
+  delay(700); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("last night...");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("You gave me your");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("Word! Dont worry");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("About her, you");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("may love her now");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("but you loved me");
+  lcd_1.setBacklight(1);
+  delay(1000); // Wait for 500 millisecond(s)
+  lcd_1.clear();
+    lcd_1.setCursor(0, 1);
+  lcd_1.print("First!!!!");
+  lcd_1.setBacklight(1);
+  delay("5000")
+}
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are o
+
+hi bro void function so so my give a fucks are on vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation and i cant relate to desperation And lcd_1.setBacklight()n vacation  n vacation and i cant relate to desperation And lcd_1.setBacklight()
